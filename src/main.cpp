77d5082a8eba6f69208dca88c065e209daaf3581
Ben Agricola <ben+git@agrico.la>
2025-11-19 00:04:34 +0000

    Phase 1 Complete: USB Serial and Basic Echo

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,24 @@
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
+  // Wait for serial connection for up to 5 seconds
+  unsigned long start = millis();
+  while (!Serial && millis() - start < 5000);
+  Serial.println("MnB Ultralight N17 Firmware Initialized");
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
-  Serial.println("Blink");
+  // Heartbeat
+  static unsigned long last_blink = 0;
+  if (millis() - last_blink > 500) {
+    last_blink = millis();
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  }
+
+  // Simple echo for testing
+  if (Serial.available()) {
+    char c = Serial.read();
+    Serial.print("Echo: ");
+    Serial.println(c);
+  }
 }
