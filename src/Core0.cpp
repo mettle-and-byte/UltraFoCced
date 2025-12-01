1b5e66c310f9c922f4b9bf03cd30b4d7cb469cf2
Ben Agricola <ben+git@agrico.la>
2025-12-01 08:46:27 +0000

    Re-add PID settings
    Signed-off-by: Ben Agricola <ben+git@agrico.la>

--- src/Core0.cpp
+++ src/Core0.cpp
@@ -61,7 +61,24 @@ void Core0::setup() {
     motor.torque_controller = TorqueControlType::voltage; // Voltage Control (FOC)
     motor.voltage_limit = max_voltage; // Torque Limit
     motor.current_limit = motor_max_current_ma / 1000.0f; // Current Limit (Virtual)
-    motor.PID_velocity.limit = motor.current_limit; // Limit PID output to physical current limit
+
+    // Velocity PID
+    motor.PID_velocity.P = 0.5;
+    motor.PID_velocity.I = 10.0;
+    motor.PID_velocity.D = 0.0;
+    motor.PID_velocity.output_ramp = 1000.0;
+    motor.PID_velocity.limit = max_voltage;
+    motor.LPF_velocity.Tf = 0.1;
+
+    // Angle PID
+    motor.P_angle.P = 20.0;
+    motor.P_angle.I = 0.0;
+    motor.P_angle.D = 0.0;
+    motor.P_angle.output_ramp = 0.0;
+    motor.P_angle.limit = 200.0; // Max Velocity
+    motor.LPF_angle.Tf = 0.0;
+
+    motor.phase_resistance = motor_phase_resistance;
 
 
     delay(5000);
